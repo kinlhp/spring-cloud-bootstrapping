730afbdd8d4628767198226aa4d30e42a536365d
kinlhp <lhp.kin@gmail.com>
2019-11-08 19:48:23 -0500

    Discovery server

--- spring-cloud-bootstrapping-project/spring-cloud-bootstrapping-configuration/src/main/java/com/kinlhp/example/spring/cloud/bootstrapping/configuration/Application.java
+++ spring-cloud-bootstrapping-project/spring-cloud-bootstrapping-configuration/src/main/java/com/kinlhp/example/spring/cloud/bootstrapping/configuration/Application.java
@@ -3,13 +3,15 @@ package com.kinlhp.example.spring.cloud.bootstrapping.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
-	public static final void main(final String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static final void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
